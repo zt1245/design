2b4aea14f9b4326e3bd68ee1f4f15e816d0a9249
zhuoting <zt1245@aliyun.com>
2019-01-19 15:02:21 +0800

    'init'

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    123
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/views/public/header.vue
@@ -0,0 +1,3 @@
+<template>
+	<h2>123</h2>
+</template>

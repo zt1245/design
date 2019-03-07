54519acb4e37fb72d0ac64af135c7cbc345e2b81
zhuoting <zt1245@aliyun.com>
2019-03-07 18:56:08 +0800

    'my'

--- design/src/components/My.vue
+++ design/src/components/My.vue
@@ -2,7 +2,14 @@
   <div class="my">
     <div class="myInfo">
       <div class="user_box container">
-        <div class="portrait"></div>
+        <div class="portrait">
+          <img src="../../static/images/user_head.jpeg" alt="">
+          <div class="right">
+            <p>昵称：木子</p>
+            <p>手机号：1585933546</p>
+            <p>收货地址：cfhufhukfsadfmahfahkh</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -21,6 +28,22 @@ export default {
     padding: 70px 0 20px;
     .user_box {
       background: #ffffff;
+    }
+    .portrait {
+      padding: 50px;
+      background: #ffffff;
+      display: flex;
+      img {
+        width: 200px;
+        height: 200px;
+      }
+      .right {
+        margin-left: 50px;
+        p {
+          text-align: left;
+          line-height: 30px;
+        }
+      }
     }
   }
 }

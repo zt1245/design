1eb5ac88762fe1d61ee412964c2c739a58156745
zhuoting <zt1245@aliyun.com>
2019-03-30 21:55:28 +0800

    个人中心地址页面

--- design/src/views/my/ReciveAdd.vue
+++ design/src/views/my/ReciveAdd.vue
@@ -6,8 +6,12 @@
         <i>+</i>
         <p>添加新地址</p>
       </li>
-      <li>
-        
+      <li v-for="(item,index) in adList"
+        :key="index">
+        <p>{{ item.name }}</p>
+        <p>{{ item.phone }}</p>
+        <p>{{ item.province }} {{ item.city }} {{ item.area }}{{ item.addr }}</p>
+        <div><span>修改</span><span>删除</span></div>
       </li>
     </ul>
   </div>
@@ -15,6 +19,28 @@
 
 <script>
 export default {
+  data () {
+    return {
+      adList: []
+    }
+  },
+  methods: {
+    addSel () {
+      var uname = localStorage.getItem('uname')
+      this.axios.post('http://localhost:3001/selAdd', {
+        uname
+      }).then((res) => {
+        if (res.data.code === 2) {
+          this.adList = res.data.result
+        } else {
+          alert('查询失败，请稍后再试！')
+        }
+      })
+    }
+  },
+  mounted () {
+    this.addSel()
+  }
 }
 </script>
 
@@ -31,12 +57,36 @@ export default {
   }
   ul {
     display: flex;
+    width: 100%;
+    flex-wrap: wrap;
     li {
-      width: 33%;
+      width: 30%;
       border: 1px solid #DCDCDC;
       margin-right: 16px;
+      padding: 33px 13px 11px 21px;
+      margin-bottom: 20px;
+      p:nth-child(1) {
+        color: #3E3E3F;
+        font-size: 18px;
+        margin-bottom: 35px;
+      }
+      p:nth-child(2),p:nth-child(3) {
+        color: #7B7B7B;
+        font-size: 14px;
+        margin-bottom: 14px;
+      }
+      div {
+        padding-top: 15px;
+        span {
+          color: #F80000;
+        }
+        span:nth-child(2) {
+          margin-left: 50px;
+        }
+      }
     }
     li:nth-child(1) {
+      cursor: pointer;
       i {
         width: 42px;
         height: 42px;
